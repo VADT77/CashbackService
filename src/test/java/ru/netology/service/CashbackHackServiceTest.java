32c795438da27f8ac5558caeda9bdbbc14a00023
VADT77 <turanosoff@gmail.com>
2023-02-05 18:38:16 +0300

    testngtest

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,60 @@
+package ru.netology.service;
+
+import static org.testng.Assert.*;
+
+import org.testng.annotations.Test;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemain999() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 1;
+        int amount = 999;
+        int result = service.remain(amount);
+
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void testRemain700() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 300;
+        int amount = 700;
+        int result = service.remain(amount);
+
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void testRemain1000() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 0;
+        int amount = 1000;
+        int result = service.remain(amount);
+
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void testRemain1001() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 999;
+        int amount = 1001;
+        int result = service.remain(amount);
+
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void testRemain1() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 999;
+        int amount = 1;
+        int result = service.remain(amount);
+
+        assertEquals(result, expected);
+    }
+
+}
